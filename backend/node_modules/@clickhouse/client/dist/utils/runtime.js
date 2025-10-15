8155caabfd52ab8959dfb3df8367dac94d412324
GitMaster-69 <23b144@psgitech.ac.in>
2025-10-15 18:58:40 +0530

    started with backend server

---
+++ backend/node_modules/@clickhouse/client/dist/utils/runtime.js
@@ -0,0 +1,64 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Runtime = void 0;
+const os = __importStar(require("node:os"));
+const version_1 = __importDefault(require("../version"));
+/** Indirect export of package version and node version for easier mocking since Node.js 22.18 */
+class Runtime {
+}
+exports.Runtime = Runtime;
+Object.defineProperty(Runtime, "package", {
+    enumerable: true,
+    configurable: true,
+    writable: true,
+    value: version_1.default
+});
+Object.defineProperty(Runtime, "node", {
+    enumerable: true,
+    configurable: true,
+    writable: true,
+    value: process.version
+});
+Object.defineProperty(Runtime, "os", {
+    enumerable: true,
+    configurable: true,
+    writable: true,
+    value: os.platform()
+});
+//# sourceMappingURL=runtime.js.map
